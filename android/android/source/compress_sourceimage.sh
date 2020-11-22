c8e858b099080debaf45a5f40e47924838b865ec
ittat <ittat@live.com>
2020-11-22 08:48:33 +0800

    [Delete]

--- android/android/source/compress_sourceimage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e 
-        cd ~ 
-        zstd  --fast=7 ${sourceimage}.dmg.sparseimage
-        rm ${sourceimage}.dmg.sparseimage
-        ls -al ${sourceimage}.dmg.sparseimage*
--- android/android/source/upload_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e 
-        df -h
-        rclone copy ~/${sourceimage}.dmg.sparseimage.zst itd:ci/${remotepath}
-        sudo rm -rf ${sourceimage}.dmg.sparseimage.zst
-        rclone copy ~/${outimage}.dmg.sparseimage itd:ci/${remotepath}
-        sudo rm -rf ${outimage}.dmg.sparseimage
-        df -h
