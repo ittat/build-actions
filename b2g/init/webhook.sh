f582a3708524516db02ad677ed6ef5bcb0c18499
ittat <ittat@live.com>
2021-03-06 10:33:13 +0800

    Update webhook.sh

--- b2g/init/webhook.sh
+++ b2g/init/webhook.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 set -e
 
-curl -H "Authorization: token ${GIT_ACCESS_TOKEN}"   --request POST  \
-      --data '{"event_type": "'$1'", \
-      "client_payload": { \ 
-        "device_name":"'${device_name}'", \
-        "gecko_version":"'${gecko_version}'", \
-        "device_arch":"'${device_arch}'", \
-        "repo_dispatches":"'${repo_dispatches}'", \
-        "build_device_tag":"'${build_device_tag}'", \
-        "b2g_source":"'${b2g_source}'", \
-        "b2g_branch":"'${b2g_branch}'", \
-        "actions_branch":"'${actions_branch}'" \
-        }}'  ${repo_dispatches}
+curl -H "Authorization: token ${GIT_ACCESS_TOKEN}"   --request POST  --data '{"event_type": "'$1'", "client_payload": { "device_name":"'${device_name}'", "gecko_version":"'${gecko_version}'", "device_arch":"'${device_arch}'", "repo_dispatches":"'${repo_dispatches}'", "build_device_tag":"'${build_device_tag}'", "b2g_source":"'${b2g_source}'", "b2g_branch":"'${b2g_branch}'", "actions_branch":"'${actions_branch}'" }}'  ${repo_dispatches}
