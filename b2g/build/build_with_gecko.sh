48f78be7db4741561d6ee9576aaf0cee586302ec
ittat <ittat@live.com>
2021-03-06 10:34:49 +0800

    Use phh's GSI
    Use phh's GSI

--- b2g/build/build_with_gecko.sh
+++ b2g/build/build_with_gecko.sh
@@ -9,16 +9,14 @@ set -e
         rclone copy itd:ci/${remotepath}/${gecko_version} ./
           
         ####  build
+        cd ${work}/B2G
         export CCACHE_DIR=~/.ccache
         /usr/local/bin/ccache  -M 20G
         /usr/local/bin/ccache -s
         export USE_CCACHE=1
-        cd ${work}/B2G
         export DISABLE_SOURCES_XML=true
         export USE_PREBUILT_B2G=1
         #export OUT_DIR_COMMON_BASE=${out_work}
         export PREFERRED_B2G="${work}/pre-gecko/${gecko_version}"
-        
         export SKIP_ABI_CHECKS=true
-        
-        gtimeout 245m  ./build.sh -j16 systemimage
+        gtimeout 245m  ./build-gsi.sh ${build_device_tag} systemimage
