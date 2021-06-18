cf94a5f7253325693a7dda025c0e59cd86f16bdb
ittat <ittat@live.com>
2021-06-18 22:51:38 +0800

    Update build_gecko.sh

--- b2g/build/build_gecko.sh
+++ b2g/build/build_gecko.sh
@@ -5,7 +5,7 @@ cd ~/gecko-b2g
 export SHELL=/bin/bash
 sudo apt update
 export LOCAL_NDK_BASE_URL='ftp://ftp.kaiostech.com/ndk/android-ndk'
-./mach bootstrap --no-interactive --application-choice 'GeckoView/Firefox for Android'
+#./mach bootstrap --application-choice 'GeckoView/Firefox for Android'
 df -h
 
 df -h
