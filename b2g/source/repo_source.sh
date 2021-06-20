fbef229825129915a813a6f68681030dc7834fc6
ittat <ittat@live.com>
2021-06-20 02:29:02 +0000

    update

--- b2g/source/repo_source.sh
+++ b2g/source/repo_source.sh
@@ -6,5 +6,5 @@ git clone ${b2g_source} -b ${b2g_branch}  --depth=1
 cd ./B2G
 echo Download ...
 #TODO
-GITREPO=https://github.com/b2g-gsi/manifests BRANCH=${b2g_branch}  REPO_INIT_FLAGS="--depth=1" REPO_SYNC_FLAGS=" -j128 --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune" ./config.sh b2g_gsi 
+GITREPO=https://github.com/b2gos/manifests BRANCH=${b2g_branch}  REPO_INIT_FLAGS="--depth=1" REPO_SYNC_FLAGS=" -j128 --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune" ./config.sh b2g_gsi 
 df -h
