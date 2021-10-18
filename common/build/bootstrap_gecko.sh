3912ad10f0a89823e95dfde51f060e921d8f8437
ittat <ittat@live.com>
2021-10-18 08:42:25 +0800

    GeckoView/Firefox for Android by default

--- common/build/bootstrap_gecko.sh
+++ common/build/bootstrap_gecko.sh
@@ -4,5 +4,5 @@ set -e
 cd ~/gecko-b2g
 export SHELL=/bin/bash
 sudo apt update
-export LOCAL_NDK_BASE_URL='ftp://ftp.kaiostech.com/ndk/android-ndk'
-#./mach bootstrap --application-choice 'GeckoView/Firefox for Android'
+#export LOCAL_NDK_BASE_URL='ftp://ftp.kaiostech.com/ndk/android-ndk'
+./mach bootstrap --application-choice 'GeckoView/Firefox for Android'
