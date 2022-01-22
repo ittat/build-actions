d784d135f42ad5f8a30501a1d105831ddb54e77b
ittat <ittat@live.com>
2022-01-22 10:50:59 +0800

    Update download_kaios_ndk.sh

--- common/source/download_kaios_ndk.sh
+++ common/source/download_kaios_ndk.sh
@@ -2,4 +2,8 @@
 set -e 
 
 cd ~
+git clone https://github.com/mozilla/gecko-dev -b master --depth=1
+cd ~/gecko-dev
+./mach bootstrap --application-choice 'GeckoView/Firefox for Android'
+cd ~
 curl https://packages.preprod.kaiostech.com/ndk/v7/mozbuild.tar.bz2 | tar -C ${HOME}/ -xj
