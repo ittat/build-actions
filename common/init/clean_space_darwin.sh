c7e9953aedaa59bf4c12c7564ab0a223d9fa7fdd
ittat <ittat@live.com>
2022-03-19 20:59:12 +0800

    Update clean_space_darwin.sh

--- common/init/clean_space_darwin.sh
+++ common/init/clean_space_darwin.sh
@@ -3,8 +3,8 @@ set -e
 ls -al -h /Applications 
 df -h
 sudo rm -r  /Applications/Xcode_10*
-#sudo rm -r  /Applications/Xcode_12*
-sudo rm -r  /Applications/Xcode_11*
+sudo rm -r  /Applications/Xcode_12*
+#sudo rm -r  /Applications/Xcode_11*
 # sudo rm -r  /Applications/Xcode_13*
 #Firefox* Julia* Microsoft* R* Visual* 
 ls -al -h /Applications
