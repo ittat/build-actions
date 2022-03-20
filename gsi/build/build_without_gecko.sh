423c78d2b5d33d45b07bbabf4e484430f542fa9b
ittat <ittat@live.com>
2022-03-20 11:32:30 +0800

    Update build_without_gecko.sh

--- gsi/build/build_without_gecko.sh
+++ gsi/build/build_without_gecko.sh
@@ -11,6 +11,7 @@ export DISABLE_SOURCES_XML=true
 export PREFERRED_B2G=${work}/b2g-dummy.tar.bz2
 export USE_PREBUILT_B2G=1
 export SKIP_ABI_CHECKS=true
+export ANDROID_NDK="${HOME}/.mozbuild/android-ndk-r21d"
 #gsi
 gtimeout 245m  ./build-gsi.sh ${build_device_tag} systemimage
 df -h
