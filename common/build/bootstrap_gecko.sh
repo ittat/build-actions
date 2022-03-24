396b028628140bb0412b82c274433599941c684b
ittat <ittat@live.com>
2022-03-24 21:12:27 +0800

    Update bootstrap_gecko.sh

--- common/build/bootstrap_gecko.sh
+++ common/build/bootstrap_gecko.sh
@@ -7,4 +7,4 @@ set -e
 #export LOCAL_NDK_BASE_URL='ftp://ftp.kaiostech.com/ndk/android-ndk'
 # ./mach bootstrap --application-choice 'GeckoView/Firefox for Android'
 wget -q https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py -O bootstrap.py 
-echo 4 | python3 bootstrap.py --no-interactive
+python3 bootstrap.py --no-interactive --application-choice=mobile_android
