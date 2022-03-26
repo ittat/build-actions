8f5342c2a16288062bbfae569fbe0f17cc87cb01
ittat <ittat@live.com>
2022-03-26 10:17:06 +0800

    u

--- common/build/bootstrap_gecko.sh
+++ common/build/bootstrap_gecko.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/expect -f
 set -e 
 
 # cd ~/gecko-b2g
@@ -8,16 +8,12 @@ set -e
 # ./mach bootstrap --application-choice 'GeckoView/Firefox for Android'
 #wget -q https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py -O bootstrap.py 
 #python3 bootstrap.py --no-interactive --application-choice=mobile_android
-brew install expect
-git clone https://github.com/mozilla/gecko-dev --depth=1
-cd ./gecko-dev  
-export MOZBUILD_STATE_PATH=~/.mozbuild
 
 
 spawn  ./mach  --no-interactive bootstrap --application-choice=mobile_android
 
 expect {
-    "*[A]ll*" {exp_send "All\r";exp_continue}
+    "*replace android-ndk-r21d/CHANGELOG.md? [y]es, [n]o, [A]ll, [N]one, [r]ename:" {exp_send "A\r";exp_continue}
   }
 expect eof
 
