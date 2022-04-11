80b24c9b041f6fc283c6b61f6f2b7b2957e10c6e
ittat <ittat@live.com>
2022-04-11 22:37:11 +0800

    Update download_kaios_ndk.sh

--- gsi/full/download_kaios_ndk.sh
+++ gsi/full/download_kaios_ndk.sh
@@ -6,7 +6,14 @@ cd ~
 # cd ~/gecko-dev
 # ./mach  --no-interactive bootstrap --application-choice 'GeckoView/Firefox for Android'
 # cd ~
+
+
+cd ~/mozbuild
+curl https://github.com/b2g-gsi/build-actions/releases/download/ndk-macos/android-ndk-0-darwin-x86_64.tar.bz2 | tar -C ./ -xj
+
 mkdir ~/old
 curl https://packages.preprod.kaiostech.com/ndk/v7/mozbuild.tar.bz2 | tar -C ${HOME}/old -xj
+
+https://github.com/b2g-gsi/build-actions/releases/download/ndk-macos/android-ndk-0-darwin-x86_64.tar.bz2
 cp -r ~/old/.mozbuild/android-ndk-r21d/toolchains/llvm/prebuilt/linux-x86_64 ~/.mozbuild/android-ndk-r21d/toolchains/llvm/prebuilt
 
